afefaab187637ffee64221a018a0bbe3d1f6a764
shafiurrahman1241 <shafiurrahman1241@gmail.com>
2020-06-29 20:53:49 +0600

    NEW ADDITION

---
+++ module-4/SpeakGoodBye.js
@@ -0,0 +1,12 @@
+// NOTE! The steps in this file are basically identical to the ones you
+// performed in the SpeakHello.js file.
+
+(function (window) {
+  var byeSpeaker = new Object();
+  var speakWord = "Good Bye";
+
+  byeSpeaker.speak = function speak(name) {
+    console.log(speakWord + " " + name);
+  }
+  window.byeSpeaker = byeSpeaker;
+})(window);
